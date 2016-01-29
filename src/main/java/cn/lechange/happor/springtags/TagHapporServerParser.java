3e847af9aa78f70b2361d5ad57ab59a7ada744a6
Ping.X <netten2005@163.com>
2016-01-29 11:20:58 +0800

    add tag happor:auto-search

--- src/main/java/cn/lechange/happor/springtags/TagHapporServerParser.java
+++ src/main/java/cn/lechange/happor/springtags/TagHapporServerParser.java
@@ -1,5 +1,6 @@
 package cn.lechange.happor.springtags;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -13,11 +14,16 @@ import org.springframework.util.xml.DomUtils;
 import org.w3c.dom.Element;
 
 import cn.lechange.happor.HapporWebserver;
+import cn.lechange.happor.controller.HttpController;
+import cn.lechange.happor.utils.PackageUtil;
 
 public class TagHapporServerParser extends AbstractSimpleBeanDefinitionParser {
 
 	private static Logger logger = Logger
 			.getLogger(TagHapporServerParser.class);
+	
+	private ParserContext parserContext;
+	private List<String> clazzList = new ArrayList<String>();
 	
 	@Override
 	protected String resolveId(Element element,
@@ -28,6 +34,8 @@ public class TagHapporServerParser extends AbstractSimpleBeanDefinitionParser {
 	@Override
 	protected void doParse(Element element, ParserContext parserContext,
 			BeanDefinitionBuilder builder) {
+		this.parserContext = parserContext;
+		
 		int port = Integer.valueOf(element.getAttribute("port"));
 		int timeout = Integer.valueOf(element.getAttribute("timeout"));
 		int maxHttpSize = Integer.valueOf(element.getAttribute("maxHttpSize"));
@@ -42,45 +50,32 @@ public class TagHapporServerParser extends AbstractSimpleBeanDefinitionParser {
 		Element handler = DomUtils.getChildElementByTagName(element, "handler");
 		if (handler != null) {
 			String clazz = handler.getAttribute("class");
-			String name = "handler";
-			try {
-				BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
-						.rootBeanDefinition(Class.forName(clazz));
-				BeanDefinitionHolder holder = new BeanDefinitionHolder(
-						beanDefinitionBuilder.getBeanDefinition(), name, null);
-				registerBeanDefinition(holder, parserContext.getRegistry());
-			} catch (ClassNotFoundException e) {
-				logger.error(e.getMessage());
-			}
+			addBean("handler", clazz);
 		}
 
 		Element controllers = DomUtils.getChildElementByTagName(element, "controllers");
 		if (controllers != null) {
-			String pack = controllers.getAttribute("package");
+			String packageName = controllers.getAttribute("package");
 			List<Element> list = DomUtils.getChildElementsByTagName(controllers, "controller");
-			int i = 0;
 			for (Element controller : list) {
-				String clazz = controller.getAttribute("class");
-				if (pack != null) {
-					clazz = pack + "." + clazz;
-				}
+				String clazz = packageName + "." + controller.getAttribute("class");
 				String method = controller.getAttribute("method");
 				String uriptn = controller.getAttribute("uriptn");
-				String name = "controller#" + i + "_" + clazz;
-				i++;
-				try {
-					BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
-							.rootBeanDefinition(Class.forName(clazz));
-					BeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
-					beanDefinition.getPropertyValues().addPropertyValue("method", method);
-					beanDefinition.getPropertyValues().addPropertyValue("uriPattern", uriptn);
-					beanDefinition.setScope("prototype");
-					BeanDefinitionHolder holder = new BeanDefinitionHolder(
-							beanDefinition, name, null);
-					registerBeanDefinition(holder, parserContext.getRegistry());
-				} catch (ClassNotFoundException e) {
-					logger.error(e.getMessage());
+				addController(clazz, method, uriptn);
+			}
+			Element autoSearch = DomUtils.getChildElementByTagName(controllers, "auto-search");
+			if (autoSearch != null) {
+				String defaultClazz = packageName + "." + autoSearch.getAttribute("defaultClass");
+				List<String> autoList = getControllersFromPackage(packageName);
+				for (String clazz : autoList) {
+					if (clazzList.contains(clazz)) {
+						continue;
+					} else if (defaultClazz.equals(clazz)) {
+						continue;
+					}
+					addController(clazz);
 				}
+				addController(defaultClazz);
 			}
 		}
 
@@ -89,6 +84,60 @@ public class TagHapporServerParser extends AbstractSimpleBeanDefinitionParser {
 	@Override
 	protected Class<?> getBeanClass(Element element) {
 		return HapporWebserver.class;
+	}
+	
+	private void addBean(String name, String clazz) {
+		try {
+			BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
+					.rootBeanDefinition(Class.forName(clazz));
+			BeanDefinitionHolder holder = new BeanDefinitionHolder(
+					beanDefinitionBuilder.getBeanDefinition(), name, null);
+			registerBeanDefinition(holder, parserContext.getRegistry());
+		} catch (ClassNotFoundException e) {
+			logger.error(e.getMessage());
+		}
+	}
+	
+	private void addController(String clazz, String method, String uriPattern) {
+		String name = "controller#" + clazzList.size() + "_" + clazz;
+		try {
+			BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
+					.rootBeanDefinition(Class.forName(clazz));
+			BeanDefinition beanDefinition = beanDefinitionBuilder.getBeanDefinition();
+			if (method != null) {
+				beanDefinition.getPropertyValues().addPropertyValue("method", method);
+			}
+			if (uriPattern != null) {
+				beanDefinition.getPropertyValues().addPropertyValue("uriPattern", uriPattern);
+			}
+			beanDefinition.setScope("prototype");
+			BeanDefinitionHolder holder = new BeanDefinitionHolder(
+					beanDefinition, name, null);
+			registerBeanDefinition(holder, parserContext.getRegistry());
+			clazzList.add(clazz);
+		} catch (ClassNotFoundException e) {
+			logger.error(e.getMessage());
+		}
+	}
+	
+	private void addController(String clazz) {
+		addController(clazz, null, null);
+	}
+	
+	private List<String> getControllersFromPackage(String packageName) {
+		List<String> list = PackageUtil.getClassName(packageName);
+		List<String> controllerList = new ArrayList<String>();
+		for (String clazz : list) {
+			clazz = clazz.substring(clazz.indexOf(packageName));
+			try {
+				if (HttpController.class.isAssignableFrom(Class.forName(clazz))) {
+					controllerList.add(clazz);
+				}
+			} catch (ClassNotFoundException e) {
+				logger.error(e.getMessage());
+			}
+		}
+		return controllerList;
 	}
 
 }
